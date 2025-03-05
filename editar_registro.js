e67141bd2842ca18afe11b6e3e69f4e8ee0b83cb
felipevacchiani <70178148+felipevacchiani@users.noreply.github.com>
2025-03-05 12:59:45 -0300

    Add files via upload

--- editar_registro.js
+++ editar_registro.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", cargarRegistros);
 
 function cargarRegistros() {
-    fetch('https://script.google.com/macros/s/AKfycbyeymwR5AEFbFMXDenpqBBFrsjHP5GbalV2XNai78k_IqhqyPlDoHjfNPMHFzUzVdL6/exec')
+    fetch('https://script.google.com/macros/s/AKfycbyEvGn6nmN-L89vvOaL7b-YBv8lZaca_aCHMW7537eidUZYFk8V38twg_eOxWAezZUl/exec')
         .then(response => response.json())
         .then(data => {
             let tbody = document.querySelector("#tablaRegistros tbody");
